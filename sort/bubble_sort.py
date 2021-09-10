a26c5a81cc7dd30d2588e445cac10ac259a5afba
Dada Ahn <71164591+lazyduo@users.noreply.github.com>
2021-09-10 13:30:27 +0900

    add optimized version

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -10,12 +10,30 @@ def bubbleSort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
+                
+def bubbleSort_opt(arr):
+    n = len(arr)
+
+    # Number of step
+    for i in range(n):
+        swapped = False
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+
+            if swapped == False:
+                break
 
 if __name__ == '__main__':
     arr = [2, 34, 56, 3, 1, 2, 45]
     print('arr : {}'.format(arr))
 
     bubbleSort(arr)
+    # bubbleSort_opt(arr)
     print(f'Bubble Sorted Array is: {arr}')
 
 ## OUTPUT
