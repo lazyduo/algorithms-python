2cb91cc6202394e3c47cf0f70539a3f5ae5803da
Dada Ahn <71164591+lazyduo@users.noreply.github.com>
2021-09-10 11:54:25 +0900

    Create bubble_sort.py

---
+++ sort/bubble_sort.py
@@ -0,0 +1,23 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    # Number of step
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+if __name__ == '__main__':
+    arr = [2, 34, 56, 3, 1, 2, 45]
+    print('arr : {}'.format(arr))
+
+    bubbleSort(arr)
+    print(f'Bubble Sorted Array is: {arr}')
+
+## OUTPUT
+# arr : [2, 34, 56, 3, 1, 2, 45]
+# Bubble Sorted Array is: [1, 2, 2, 3, 34, 45, 56]
