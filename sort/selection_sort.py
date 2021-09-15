484800dc3116917947197ac129a285462c68c6e7
Dada Ahn <71164591+lazyduo@users.noreply.github.com>
2021-09-15 16:15:26 +0900

    Create selection_sort.py

---
+++ sort/selection_sort.py
@@ -0,0 +1,49 @@
+def slsectionSort(arr):
+    for i in range(len(arr)):
+        
+        min_idx = i
+        # find the minimum element idx
+        # in remaining unsorted array
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        
+        # Swap
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+def stableSlsectionSort(arr):
+    for i in range(len(arr)):
+        
+        min_idx = i
+        # find the minimum element idx
+        # in remaining unsorted array
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        
+        # instead Swapping, insert min to right place
+        key = arr[min_idx]
+        while min_idx > i:
+            arr[min_idx] = arr[min_idx - 1]
+            min_idx -= 1
+        arr[i] = key
+
+
+if __name__ == '__main__':
+    arr = [2, 34, 56, 3, 1, 2, 45]
+    print('arr : {}'.format(arr))
+
+    slsectionSort(arr)
+    print(f'Slection Sorted Array is: {arr}')
+
+    arr2 = [2, 34, 56, 3, 1, 2, 45]
+    print('arr2 : {}'.format(arr2))
+
+    stableSlsectionSort(arr2)
+    print(f'Stable Slection Sorted Array is: {arr2}')
+
+## OUTPUT
+# arr : [2, 34, 56, 3, 1, 2, 45]
+# Slection Sorted Array is: [1, 2, 2, 3, 34, 45, 56]
+# arr2 : [2, 34, 56, 3, 1, 2, 45]
+# Stable Slection Sorted Array is: [1, 2, 2, 3, 34, 45, 56]
